6fe5a1c4c71c27c17f8d9fbb676eb281c42a6269
m.0830295514 <m.0830295514@gmail.com>
2025-12-16 02:15:34 +0000

    Detail Update2

--- lib/model/recipe.dart
+++ lib/model/recipe.dart
@@ -7,11 +7,13 @@ class Recipe {
   Recipe(this.imgUrl, this.imgTitle, this.detail);
 
   static List<Recipe> samples = [
-    Recipe('assets/images/1.webp', 'Pad Thai', 'Pad Thai is a stir-fried rice noodle dish commonly served as street food in Thailand.'),
-    Recipe('assets/images/2.webp', 'Green Thai Fish Curry', 'Green Thai Fish Curry is a flavorful and aromatic dish made with fresh fish, green curry paste, coconut milk, and a variety of herbs and spices.'),
-    Recipe('assets/images/3.webp', 'Pumpkin Curry', 'Pumpkin Curry is a creamy and comforting dish made with pumpkin, coconut milk, and aromatic spices.'),
-    Recipe('assets/images/4.webp', 'Northern Larb', 'Northern Larb is a spicy and refreshing salad made with minced meat, herbs, and lime juice.'),
+    Recipe('assets/images/1.webp', 'Green Thai Fish Curry',
+        ' Green Thai Fish Curry is a flavorful and aromatic dish made with fresh fish, green curry paste, coconut milk, and a variety of herbs and spices.'),
+    Recipe('assets/images/2.webp', 'Pad Thai',
+        'Pad Thai is a stir-fried rice noodle dish commonly served as street food in Thailand.'),
+    Recipe('assets/images/3.webp', 'Pumpkin Curry',
+        'Pumpkin Curry is a creamy and comforting dish made with pumpkin, coconut milk, and aromatic spices.'),
+    Recipe('assets/images/4.webp', 'Northern Larb',
+        'Northern Larb is a spicy and refreshing salad made with minced meat, herbs, and lime juice.'),
   ];
- 
- 
 }
--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-
 import 'package:myapp/model/recipe.dart';
-
 class RecipeDetail extends StatelessWidget {
   final Recipe recipe;
 
